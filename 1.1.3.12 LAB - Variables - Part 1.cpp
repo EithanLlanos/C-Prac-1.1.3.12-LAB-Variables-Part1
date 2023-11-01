86494430e004d96a74f8cfbae2a7e430c4ce1ba8
EithanLlanos <eithanllanosb@gmail.com>
2023-11-01 13:34:16 -0500

    first commit

---
+++ 1.1.3.12 LAB - Variables - Part 1.cpp
@@ -0,0 +1,26 @@
+//Scenario
+//Check the program in the editor.Find all possible compilation errors and logic errors.Fix them.
+//
+//Your version of the program must print the same result as the expected output.Before you use the compiler, try to find the errors only by manual code analysis.
+//
+//If you want to improve the variable names, then do so, but remember that variable names have to be as descriptive as possible, and also as short as possible.
+/////////////////////////////////////////////////////////////////////////////////////////////
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int simpleVari able = 10;
+//	printf("The value of ten is: %d \n", otherVariable);
+//	return 0;
+//}
+//
+//Expected output
+//The value of ten is : 10
+
+#include <stdio.h>
+
+int main() {
+	int simpleVariable = 10;
+	printf("The value of ten is: %d \n", simpleVariable);
+	return 0;
+}
